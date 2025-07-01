796117ef024a4e675eb4f0af7ee66061a5e01fe8
Anindya Ganguly <136730996+anindya-19@users.noreply.github.com>
2025-07-01 14:57:23 +0530

    Refactor encryption and decryption functions; improve input handling and flow

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -4,24 +4,47 @@ alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q'
 def encryption(text,shift):
     cipherText = ""
     for char in text:
-        position = alphabets.index(char)
-        newPosition = (position+shift) % 26
-
-        cipherText += alphabets[newPosition]
+        if char in alphabets:
+            position = alphabets.index(char)
+            newPosition = (position+shift) % 26
+            cipherText += alphabets[newPosition]
+        else:
+            cipherText += char
     print("Here's the text after encryption: ",cipherText)
 
-def decryption(text,shift):
-    pass
+def decryption(cipherText,shift):
+    plainText = ""
+    for char in cipherText:
+        if char in alphabets:
+            position = alphabets.index(char)
+            oldPosition = (position-shift) % 26
+            plainText += alphabets[oldPosition]
+        else:
+            plainText += char
+    
+    print(f"The decrypted text is: {plainText}")
 
 
+over = False
+while not over:
+    task = input("Type 'encrypt' for encryption purposes and 'decrypt' for decryption purpose: ").lower()
+    if task not in ['encrypt','decrypt']:
+        print("Please enter a valid command!! Try again")
+        continue
+    text = input("Type your message:\n").lower()
+    try:
+        shift = int(input("Enter the shift value (integer): \n"))
+    except ValueError:
+        print("The shift value must be an integer")
+        continue
 
-
-task = input("Type 'encrypt' for encryption purposes and 'decrypt' for decryption purpose: ")
-
-text = input("Type your message:\n")
-shift = int(input("Enter the shift value: \n"))
-
-if task == 'encrypt':
-    encryption(text,shift)
-elif task == 'decrypt':
-    decryption(text,shift)
+    if task == 'encrypt':
+        encryption(text,shift)
+    elif task == 'decrypt':
+        decryption(text,shift)
+    
+    again = input("Enter 'yes' to continue and 'no' to exit ")
+    
+    if again.strip().lower() == 'no':
+        over = True
+        print("Bye! Game is over now")
