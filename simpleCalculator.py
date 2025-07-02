ef5dc04d368b50292cc95075a2009f818224b907
Anindya Ganguly <136730996+anindya-19@users.noreply.github.com>
2025-07-02 18:49:31 +0530

    Fix typo in subtraction function and refactor calculator logic for better input handling

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,36 +1,61 @@
+import os
 def add(a,b):
     return a+b
-def substract(a,b):
+def subtract(a,b):
     return a-b
 def multiply(a,b):
     return a*b
 def divide(a,b):
+    if b == 0:
+        print("Error: Division by zero is not possible")
     return a/b
 
 operations_dict = {
     "+":add,
-    "-":substract,
+    "-":subtract,
     "*":multiply,
     "/":divide
 }
 
-num1 = int(input("Enter the first number: "))
-print("Available Operations: ")
-for keys in operations_dict:
-    print(keys)
-while True:
-    operator = input("Enter what Operation you want to perform:  ")
-    num2 = int(input("Enter the second number: "))
-    calculatorFunction = operations_dict[operator]
-    output = calculatorFunction(num1,num2)
-    print(f"{num1} {operator} {num2} = {output}")
+def calculator():
+    try:
+        num1 = float(input("Enter the first number: "))
+    except ValueError:
+        print("Please enter a valid integer")
+    print("Available Operations: ")
+    for keys in operations_dict:
+        print(keys)
+    continueFlag = True
+    while continueFlag:
+        operator = input("Enter what Operation you want to perform:  ").strip()
+        try:
+            num2 = float(input("Enter the second number: "))
+        except ValueError:
+            print("Please enter a valid integer")
+        calculatorFunction = operations_dict[operator]
+        output = calculatorFunction(num1,num2)
+        if output is not None:
+            print(f"{num1} {operator} {num2} = {output}")
+        else:
+            continue #division by zero case, ask for the second number again
 
-    choice = input(f"Enter 'y' to continue to continue previous operaion with {output} or 'n' to exit ").strip().lower()
+        choice = input(f"Enter 'y' to continue to continue previous operaion with {output} or 'n' to start a new calculation or 'x' to exit : ").strip().lower()
 
-    if choice == 'y':
-        num1 = output
-    else:
-        print("Bye!!")
-        break
+        if choice == 'y':
+            num1 = output
+        elif choice == 'x':
+            print("Bye!!")
+            continueFlag = False
+        elif choice == 'n':
+            continueFlag = False
+            print("Starting a new Calculation!")
+            os.system('cls')
+            calculator()
+        else:
+            print("Please enter a valid choice!")
+
+
+calculator()
+        
     
     
