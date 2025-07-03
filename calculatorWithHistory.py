19b3f83d29777c80ea12ecd791a9a7087d540a26
Anindya Ganguly <136730996+anindya-19@users.noreply.github.com>
2025-07-03 12:44:15 +0530

    Refactor calculator logic for improved error handling and code clarity

--- calculatorWithHistory.py
+++ calculatorWithHistory.py
@@ -1,3 +1,4 @@
+import os
 HISTORY_FILE = "historyCalculator.txt"
 
 def add(a,b):
@@ -9,8 +10,8 @@ def multiply(a,b):
 def divide(a,b):
     if b == 0:
         print("Division by Zero is not possible")
-    else:
-        return a / b
+        return None
+    return a / b
 
 operation_dict = {
     "+":add,
@@ -36,17 +37,48 @@ def clear_history():
 
 def save_history(equation,result):
     fp = open("HISTORY_FILE","a")
-    fp.write(equation + "=" + str(result) +"\n")
+    fp.write(f"{equation} = {result} \n")
     fp.close()
 
 def calculate(user_input):
     parts = user_input.split()
     if len(parts) != 3:
-        print("Invalid Input. Use format: number operator number (e.g 8 + 8 = 16)")
+        print("Invalid Input. Use format: number operator number (e.g 8 + 8 = 16) or use the following Commands!")
         return
-    num1 = float(parts[0])
-    op = parts[1]
-    num2 = float(parts[2])
+    try:
+        num1 = float(parts[0])
+        op = parts[1]
+        num2 = float(parts[2])
+    except ValueError:
+        print("Invalid Numbes. Please enter valid numbers! ")
+        return
 
+    if op not in operation_dict:
+        print(f"Invalid Operator '{op}'. Use one of: {', '.join(operation_dict.keys())} ")
+        return
     calculatorFunction = operation_dict[op]
     result = calculatorFunction(num1,num2)
+    
+    if result is not None:
+        if int(result) == result:
+            result = int(result)
+        print("Result: ",result)
+        save_history(user_input,result)
+    
+def main():
+    print("____Calculator (type history, clear or exit)")
+    while True:
+        user_input = input("Enter calculations (+ - * /) or command (history, clear or exit):  ").strip().lower()
+        
+        if user_input == 'exit':
+            print("Thanks For using! Have a nice day!")
+            break
+        elif user_input == 'history':
+            showHistory()
+        elif user_input == 'clear':
+            os.system('cls')
+            clear_history()
+        else:
+            calculate(user_input)
+
+main()
